bbfd0a44c03cb8b47a468464aec758bd5124aca0
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-07-02 23:44:24 +0500

    Added Req Res

---
+++ _89 Req Res/README.md
@@ -0,0 +1,7 @@
+ Response, Request and Routers
+
+ 1. Handling post and other Requests
+ 2. Chaining Requests
+ 3. Serving HTML Files
+ 4. Intalling Postman
+ 5. Express Router
---
+++ _89 Req Res/main.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static("public"))
+
+app.use((req, res, next) => {
+    console.log(`🟠 METHOD: ${req.method}, URL: ${req.url}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    console.log(`🟢 User sent a GET REQUEST!`)
+    res.send('Hello Worlds!')
+})
+
+app.get('/api', (req, res) => {
+    res.json({a: 1, b: 3, c: 3, d: 4, name: ["harry", "bro"]})
+})
+
+app.get('/index', (req, res) => {
+    console.log(`🟢 User sent a GET REQUEST!`)
+    res.sendFile('template/index.html', {root: __dirname})
+})
+
+app.post('/', (req, res) => {
+    console.log(`🟡 User sent a POST REQUEST!`, req.body)
+    res.send(`POST REQUEST!`)
+})
+
+app.put('/', (req, res) => {
+    console.log(`🔴 User sent a PUT REQUEST!`, req.body)
+    res.send(`PUT REQUEST!`)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
