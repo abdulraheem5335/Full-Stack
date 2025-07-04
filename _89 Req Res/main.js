f68ff0bafeacaf0af5e585f9888e23bf41d62f64
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-07-04 15:08:13 +0500

    Added Routes _89

--- _89 Req Res/main.js
+++ _89 Req Res/main.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const blog = require('./routes/blog.js')
+const shop = require('./routes/shop.js')
 
 app.use(express.json());
 app.use(express.static("public"))
+app.use('/blog', blog);
+app.use('/shop', shop);
 
 app.use((req, res, next) => {
     console.log(`🟠 METHOD: ${req.method}, URL: ${req.url}`);
